e7266370bb5e0bfb374c0b0ea48256b39de8a4e4
Jaishna Vardhan <jaishna.vardhan08@gmail.com>
2026-06-10 23:43:51 +0530

    PING PONG ARCADE GAME

---
+++ MainArcade.py
@@ -0,0 +1,39 @@
+from turtle import Turtle, Screen
+import time
+from paddle import Paddle
+from ball import Ball
+from scoreboardarcade import Scoreboard
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("PONG")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350,0))
+ball = Ball()
+
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+#detect collision with wall
+    if ball.ycor() > 180 or ball.ycor() < -180:
+        ball.bounce()
+    #detecting collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
+
+
+screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,19 @@
+from turtle import Screen,Turtle
+class Paddle(Turtle):
+
+    def __init__(self,pos):
+        super().__init__()
+        paddle = Turtle()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(pos)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
