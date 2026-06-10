c2752cae01bbb546f188a9683682bd65e648d604
Jaishna Vardhan <jaishna.vardhan08@gmail.com>
2026-06-10 23:45:26 +0530

    TURTLE CROSSING ROAD GAME

--- main.py
+++ main.py
@@ -1,40 +1,39 @@
-from turtle import Turtle
-STARTING_POSITIONS = [(0,0),(-20,0),(-40,0)]
-MOVE_DISTANCE = 20
-UP = 90
-DOWN = 270
-RIGHT = 0
-LEFT = 180
-class Snake:
-    def __init__(self):
-        self.segments = []
-        self.create_snake()
-        self.head = self.segments[0]
-    def create_snake(self):
-        for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
-    def move(self):
-        for seg_num in range(len(self.segments) - 1, 0, -1):
-            new_x = self.segments[seg_num - 1].xcor()
-            new_y = self.segments[seg_num - 1].ycor()
-            self.segments[seg_num].goto(new_x, new_y)
-        self.head.forward(MOVE_DISTANCE)
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
 
-    def up(self):
-        if self.head.heading() != DOWN:
-            self.segments[0].setheading(UP)
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
 
-    def down(self):
-        if self.head.heading() != UP:
-            self.head.setheading(DOWN)
-    def right(self):
-        if self.head.heading() != LEFT:
-            self.segments[0].setheading(RIGHT)
-    def left(self):
-        if self.head.heading() != RIGHT:
-            self.segments[0].setheading(LEFT)
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
 
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    #Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    #Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+
+screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,24 +1,26 @@
 from turtle import Turtle
-ALIGNMENT = "center"
-FONT = ("courier",24,"normal")
+
+FONT = ("Courier", 24, "normal")
 
 
 class Scoreboard(Turtle):
+
     def __init__(self):
         super().__init__()
-        self.score = 0
-        self.color("white")
-        self.penup()
-        self.goto(0,270)
+        self.level = 1
         self.hideturtle()
+        self.penup()
+        self.goto(-280, 250)
+        self.update_scoreboard()
 
-    def update_score(self):
-        self.write(f"score: {self.score}", align=ALIGNMENT, font=FONT)
-    def game_over(self):
-        self.goto(0,0)
-        self.write(f"GAME OVER!", align=ALIGNMENT, font=FONT)
-
-    def increase_score(self):
-        self.score += 1
+    def update_scoreboard(self):
         self.clear()
-        self.update_score()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="center", font=FONT)
